62970c584a19def92795867abb7cb17ea0e5f1f8
almaan <almaan@kth.se>
2022-04-19 22:50:35 +0200

    new synth approach

--- openproblems/tasks/spatial_decomposition/datasets/_utils.py
+++ openproblems/tasks/spatial_decomposition/datasets/_utils.py
@@ -48,8 +48,10 @@ def generate_synthetic_dataset(
         - `adata_spatial.X`: simulated counts (aggregate of sc dataset).
         - `adata_spatial.uns["sc_reference"]`: original sc adata for reference.
         - `adata_spatial.obsm["proportions_true"]`: true proportion values.
-        - `adata_spatial.obsm["n_cells"]`: number of cells from each type at every location # noqa: 501
-        - `adata_spatial.obs["proportions_true"]`: total number of cells at each location # noqa: 501
+        - `adata_spatial.obsm["n_cells"]`: number of cells from each type at
+           every location 
+        - `adata_spatial.obs["proportions_true"]`:
+           total number of cells at each location 
 
     The cell type labels are stored in adata_sc.obs["label"].
 
