d0fe6c9226a9601997ec30353bc47eda8d33b696
almaan <almaan@kth.se>
2022-04-20 08:39:58 +0200

    pancreas subset integer; comment pancreas dataset [skip actions]

--- openproblems/tasks/spatial_decomposition/datasets/_pancreas_utils.py
+++ openproblems/tasks/spatial_decomposition/datasets/_pancreas_utils.py
@@ -1,11 +1,11 @@
+import anndata as ad
 import numpy as np
 import scanpy as sc
-import anndata as ad
 
 
-def get_pancreas_integer(adata:ad.AnnData):
+def get_pancreas_integer(adata: ad.AnnData):
     is_int = ["smartseq2"]
-    is_int += ["inDrop{}".format(x) for x in range(1,5)]
+    is_int += ["inDrop{}".format(x) for x in range(1, 5)]
 
     keep = np.zeros(len(adata)).astype(bool)
 
@@ -13,7 +13,6 @@ def get_pancreas_integer(adata:ad.AnnData):
         idx = adata.obs.tech.values == tech
         keep = keep | idx
 
-    adata = adata[keep,:]
+    adata = adata[keep, :]
     adata.X = adata.X
     return adata
-
--- openproblems/tasks/spatial_decomposition/datasets/pancreas.py
+++ openproblems/tasks/spatial_decomposition/datasets/pancreas.py
@@ -1,7 +1,7 @@
 from ....data.pancreas import load_pancreas
 from ....tools.decorators import dataset
-from ._utils import generate_synthetic_dataset
 from ._pancreas_utils import get_pancreas_integer
+from ._utils import generate_synthetic_dataset
 
 
 @dataset("Pancreas (alpha=1)")
@@ -18,17 +18,17 @@ def pancreas_alpha_1(test=False, n_obs=1000):
 def pancreas_alpha_5(test=False, n_obs=1000):
     adata = load_pancreas(test=test)
     adata = get_pancreas_integer(adata
-    adata.obs["label"] = adata.obs["celltype"]
+    adata.obs["label"]=adata.obs["celltype"]
 
-    adata_spatial = generate_synthetic_dataset(adata, n_obs=n_obs, alpha=5)
+    adata_spatial=generate_synthetic_dataset(adata, n_obs=n_obs, alpha=5)
     return adata_spatial
 
 
-@dataset("Pancreas (alpha=0.5)")
+@ dataset("Pancreas (alpha=0.5)")
 def pancreas_alpha_0_1(test=False, n_obs=1000):
-    adata = load_pancreas(test=test)
-    adata = get_pancreas_integer(adata
-    adata.obs["label"] = adata.obs["celltype"]
+    adata=load_pancreas(test=test)
+    adata=get_pancreas_integer(adata
+    adata.obs["label"]=adata.obs["celltype"]
 
-    adata_spatial = generate_synthetic_dataset(adata, n_obs=n_obs, alpha=0.5)
+    adata_spatial=generate_synthetic_dataset(adata, n_obs=n_obs, alpha=0.5)
     return adata_spatial
