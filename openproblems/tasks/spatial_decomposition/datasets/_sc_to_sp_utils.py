da9d78b7b2903f94e82ba84821d4599ae57bdc81
giovp <giov.pll@gmail.com>
2022-04-24 10:33:54 +0200

    fix sparse

--- openproblems/tasks/spatial_decomposition/datasets/_sc_to_sp_utils.py
+++ openproblems/tasks/spatial_decomposition/datasets/_sc_to_sp_utils.py
@@ -62,7 +62,7 @@ def generate_synthetic_dataset(
     rng = np.random.default_rng(seed)
 
     # get single cell expression data
-    X = adata.X.A
+    X = adata.X
     if issparse(X):
         X = X.toarray()
     # get cell annotations/labels
