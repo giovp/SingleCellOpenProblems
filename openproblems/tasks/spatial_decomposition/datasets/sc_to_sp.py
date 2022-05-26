8745be5e78002a408d4e92875a0565970d10d833
giovp <giov.pll@gmail.com>
2022-05-26 14:21:05 +0200

    skip all pancreas

--- openproblems/tasks/spatial_decomposition/datasets/sc_to_sp.py
+++ openproblems/tasks/spatial_decomposition/datasets/sc_to_sp.py
@@ -3,24 +3,25 @@ from ....tools.decorators import dataset
 from ._sc_to_sp_utils import generate_synthetic_dataset
 from ._sc_to_sp_utils import get_pancreas_integer
 
-# @dataset("Pancreas (alpha=1)")
-# def pancreas_alpha_1(test=False, n_obs=100):
-#     adata = load_pancreas(test=False)
-#     adata = get_pancreas_integer(adata)
-#     adata.obs["label"] = adata.obs["celltype"]
 
-#     merged_adata = generate_synthetic_dataset(adata, n_obs=n_obs, alpha=1)
-#     return merged_adata
-
-
-@dataset("Pancreas (alpha=5)")
-def pancreas_alpha_5(test=False, n_obs=100):
+@dataset("Pancreas (alpha=1)")
+def pancreas_alpha_1(test=False, n_obs=10):
     adata = load_pancreas(test=False)
     adata = get_pancreas_integer(adata)
     adata.obs["label"] = adata.obs["celltype"]
 
-    merged_adata = generate_synthetic_dataset(adata, n_obs=n_obs, alpha=5)
+    merged_adata = generate_synthetic_dataset(adata, n_obs=n_obs, alpha=1)
     return merged_adata
+
+
+# @dataset("Pancreas (alpha=5)")
+# def pancreas_alpha_5(test=False, n_obs=100):
+#     adata = load_pancreas(test=False)
+#     adata = get_pancreas_integer(adata)
+#     adata.obs["label"] = adata.obs["celltype"]
+
+#     merged_adata = generate_synthetic_dataset(adata, n_obs=n_obs, alpha=5)
+#     return merged_adata
 
 
 # @dataset("Pancreas (alpha=0.5)")
