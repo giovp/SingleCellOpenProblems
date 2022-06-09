8203f425dba462bb12a19b6e90b96c145cf2c9c8
giovp <giov.pll@gmail.com>
2022-06-09 15:25:36 +0200

    resolve suggestions from scott

--- openproblems/tasks/spatial_decomposition/datasets/sc_to_sp.py
+++ openproblems/tasks/spatial_decomposition/datasets/sc_to_sp.py
@@ -9,13 +9,11 @@ import scanpy as sc
 @dataset("Pancreas (alpha=1)")
 def pancreas_alpha_1(test=False, n_obs=10):
     adata = load_pancreas(test=test)
-    from_cache = adata.__from_cache__
     adata = get_pancreas_integer(adata)
     sc.pp.filter_genes(adata, min_counts=10)
     adata.obs["label"] = adata.obs["celltype"]
 
     merged_adata = generate_synthetic_dataset(adata, n_obs=n_obs, alpha=1)
-    merged_adata.__from_cache__ = from_cache
     return merged_adata
 
 
